d5d9753f3e0ce4e1466cd5d24a0472cf9f50309e
Mohammed Sadiq <sadiqmmm@gmail.com>
2018-10-17 21:15:25 +0530

    Fix the login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :blogs
-  #devise_for :users
+  resources :blogs 
 
-  mount_devise_token_auth_for 'User', at: 'api/auth', controllers: { sessions: 'api/auth/sessions' }
+  devise_for :users 
 
+  namespace :api_login do 
+    mount_devise_token_auth_for 'User', at: 'api/auth', controllers: { sessions: 'api/auth/sessions' }  
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: "json"} do
     resources :blogs, only: [:index, :update]    
